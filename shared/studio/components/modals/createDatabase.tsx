4d423bb72453a1d33bfea316f5a6ec34cb564328
James Clarke <james@edgedb.com>
2022-06-28 21:53:16 +0100

    Update create database modal to use 'react-hook-form'

--- shared/studio/components/modals/createDatabase.tsx
+++ shared/studio/components/modals/createDatabase.tsx
@@ -1,11 +1,12 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
 
 import {useModal} from "@edgedb/common/hooks/useModal";
 import {ModalOverlay, Modal, ModalTextField} from "@edgedb/common/ui/modal";
 import Button from "@edgedb/common/ui/button";
 
-import {InstanceState, useInstanceState} from "../../state/instance";
+import {InstanceState} from "../../state/instance";
 
 import styles from "./modals.module.scss";
 
@@ -21,14 +22,29 @@ export default function CreateDatabaseModal({
   const navigate = useNavigate();
   const {openModal} = useModal();
 
-  const [name, setName] = useState("");
-  const [creating, setCreating] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
   const [error, setError] = useState("");
 
+  const {register, handleSubmit, formState, setFocus} = useForm<{
+    dbName: string;
+  }>({mode: "onChange"});
+
   useEffect(() => {
-    inputRef.current?.focus();
+    setFocus("dbName");
   }, []);
+
+  const onSubmit = handleSubmit(async ({dbName}) => {
+    try {
+      await instanceState.defaultConnection?.query(
+        `create database ${dbName}`
+      );
+    } catch (e) {
+      setError((e as any).toString());
+      return;
+    }
+    await instanceState.fetchInstanceInfo();
+    navigate(`${dbPagePathPrefix}/${dbName}`);
+    openModal(null);
+  });
 
   return (
     <ModalOverlay onOverlayClick={() => openModal(null)}>
@@ -38,38 +54,29 @@ export default function CreateDatabaseModal({
         actions={
           <Button
             className={styles.greenButton}
-            loading={creating}
-            disabled={!name.trim() || creating}
+            loading={formState.isSubmitting}
+            disabled={!formState.isValid || formState.isSubmitting}
             size="large"
             label={"Create database"}
-            onClick={async () => {
-              setCreating(true);
-              try {
-                await instanceState.defaultConnection?.query(
-                  `create database ${name}`
-                );
-              } catch (e) {
-                setError((e as any).toString());
-                setCreating(false);
-                return;
-              }
-              await instanceState.fetchInstanceInfo();
-              navigate(`${dbPagePathPrefix}/${name}`);
-              openModal(null);
-            }}
+            onClick={onSubmit}
           />
         }
       >
-        <div className={styles.modalBody}>
+        <form className={styles.modalBody} onSubmit={onSubmit}>
           <ModalTextField
-            ref={inputRef}
             label="Database Name"
-            value={name}
-            onChange={setName}
+            {...register("dbName", {
+              required: "Database name is required",
+              pattern: {
+                value: /^[A-Za-z]\w*$/,
+                message: "Invalid database name",
+              },
+            })}
+            error={formState.errors.dbName?.message}
           />
 
           <div className={styles.errorText}>{error}</div>
-        </div>
+        </form>
       </Modal>
     </ModalOverlay>
   );
