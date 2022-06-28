9084fb886ef8b88b662b5045e8bae0a4cf07226e
James Clarke <james@edgedb.com>
2022-06-28 21:53:53 +0100

    Rename to 'EdgeDB Local'

--- web/src/components/main/index.tsx
+++ web/src/components/main/index.tsx
@@ -1,4 +1,5 @@
-import {useNavigate, useRoutes} from "react-router-dom";
+import {useEffect} from "react";
+import {useMatch, useNavigate, useRoutes} from "react-router-dom";
 import {observer} from "mobx-react-lite";
 
 import {HeaderTab} from "@edgedb/studio/components/headerTabs";
@@ -6,18 +7,33 @@ import {HeaderInstanceIcon} from "@edgedb/studio/icons";
 
 import {useAppState} from "src/state/providers";
 
+import styles from "./main.module.scss";
+
 import InstancePage from "../instancePage";
 import DatabasePage from "../databasePage";
 
 export default observer(function Main() {
   const appState = useAppState();
   const navigate = useNavigate();
+  const match = useMatch(":databaseName/*");
+
+  const instanceName = appState.instanceState.instanceName;
+
+  useEffect(() => {
+    document.title = instanceName
+      ? `${instanceName}${
+          match ? ` / ${match.params.databaseName}` : ""
+        } | EdgeDB Local`
+      : "EdgeDB Local";
+  }, [instanceName, match]);
 
   return (
     <>
       <HeaderTab
         depth={0}
-        title={appState.instanceState.instanceName ?? ""}
+        title={
+          instanceName ?? <span className={styles.loading}>loading...</span>
+        }
         icon={<HeaderInstanceIcon />}
         mainAction={() => navigate("/")}
         items={null}
