bdeb75d25643e7bb8966acd6b402a3d666fa3962
James Clarke <james@edgedb.com>
2022-07-19 19:25:49 +0100

    Use theme switcher from docs

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -2,18 +2,16 @@ import {observer} from "mobx-react";
 
 import {HeaderTabs} from "@edgedb/studio/components/headerTabs";
 import {SessionStateControls} from "@edgedb/studio/components/sessionState";
-import {useTheme, Theme} from "@edgedb/common/hooks/useTheme";
 
 import {useAppState} from "src/state/providers";
 
 import {Logo} from "@edgedb/common/ui/icons/logo";
-import {ThemeSwitcherIcon} from "@edgedb/common/ui/icons";
+import {ThemeSwitcher} from "@edgedb/common/ui/themeSwitcher";
 
 import styles from "./header.module.scss";
 
 export default observer(function Header() {
   const appState = useAppState();
-  const [theme, setTheme] = useTheme();
 
   return (
     <div className={styles.header}>
@@ -26,15 +24,7 @@ export default observer(function Header() {
 
       <SessionStateControls />
 
-      <div
-        className={styles.themeSwitcher}
-        style={{marginLeft: "auto"}}
-        onClick={() =>
-          setTheme(theme === Theme.dark ? Theme.light : Theme.dark)
-        }
-      >
-        <ThemeSwitcherIcon />
-      </div>
+      <ThemeSwitcher className={styles.themeSwitcher} />
     </div>
   );
 });
