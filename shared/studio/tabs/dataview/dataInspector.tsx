edgedb/edgedb-ui

commit 14a7701a66eb7042de3e9f4de90ad8577b637806

Further fixes for union links handling in data explorer

shared/studio/tabs/dataview/dataInspector.tsx
@@ -338,9 +338,9 @@ const GridCell = observer(function GridCell({
   const cellEditState = edits.propertyEdits.get(cellId);
   const linkEditState = edits.linkEdits.get(cellId);
 
-  const editedLinkChange =
-    state.parentObject &&
-    edits.linkEdits.get(state.parentObject.linkId)?.changes.get(data?.id);
+  const editedLink =
+    state.parentObject && edits.linkEdits.get(state.parentObject.linkId);
+  const editedLinkChange = editedLink?.changes.get(data?.id);
 
   const value =
     cellEditState?.value !== undefined
@@ -422,10 +422,12 @@ const GridCell = observer(function GridCell({
       const countData = data?.[`__count_${field.queryName}`];
       if (countData !== null || insertedRow) {
         const counts: {[typename: string]: number} =
-          countData?.reduce((counts: any, {typename, count}: any) => {
-            counts[typename] = count;
-            return counts;
-          }, {}) ?? {};
+          field.multi || !linkEditState
+            ? countData.reduce((counts: any, {typename, count}: any) => {
+                counts[typename] = count;
+                return counts;
+              }, {})
+            : {};
 
         if (linkEditState) {
           for (const change of linkEditState.changes.values()) {
@@ -479,7 +481,12 @@ const GridCell = observer(function GridCell({
         [styles.loadingCell]: !data,
         [styles.isDeleted]:
           isDeletedRow ||
-          editedLinkChange?.kind === UpdateLinkChangeKind.Remove,
+          editedLinkChange?.kind === UpdateLinkChangeKind.Remove ||
+          (!state.parentObject?.isMultiLink &&
+            !!editedLink &&
+            (state.parentObject?.editMode
+              ? !!data?.__isLinked
+              : !editedLinkChange)),
         [styles.editableCell]:
           !isDeletedRow &&
           isEditable &&
@@ -718,12 +725,21 @@ const DataRowIndex = observer(function DataRowIndex({
 
   const editedLinkChange = editedLink?.changes.get(data?.id);
 
+  const hasLinkEdit = state.parentObject?.editMode
+    ? !!editedLinkChange ||
+      (!state.parentObject?.isMultiLink &&
+        editedLink != null &&
+        !!data?.__isLinked)
+    : state.parentObject?.isMultiLink
+    ? !!editedLinkChange
+    : !!editedLink;
+
   return (
     <>
       <div
         className={cn(styles.rowIndex, {
           [styles.active]: active,
-          [styles.hasLinkEdit]: !!editedLinkChange || dataIndex === null,
+          [styles.hasLinkEdit]: hasLinkEdit || dataIndex === null,
         })}
         style={{top: styleTop}}
         onMouseEnter={() => state.setHoverRowIndex(rowIndex)}
@@ -760,7 +776,7 @@ const DataRowIndex = observer(function DataRowIndex({
                               state.parentObject!.subtypeName ??
                                 state.parentObject!.objectTypeName,
                               state.parentObject!.fieldName,
-                              state.objectType!.name,
+                              state.objectType!,
                               data.__isLinked
                                 ? UpdateLinkChangeKind.Remove
                                 : UpdateLinkChangeKind.Add,
@@ -774,7 +790,7 @@ const DataRowIndex = observer(function DataRowIndex({
                             state.parentObject!.subtypeName ??
                               state.parentObject!.objectTypeName,
                             state.parentObject!.fieldName,
-                            state.objectType!.name,
+                            state.objectType!,
                             state.insertedRows[rowIndex]
                           );
                         }
@@ -803,7 +819,7 @@ const DataRowIndex = observer(function DataRowIndex({
                               state.parentObject!.subtypeName ??
                                 state.parentObject!.objectTypeName,
                               state.parentObject!.fieldName,
-                              state.objectType!.name,
+                              state.objectType!,
                               UpdateLinkChangeKind.Set,
                               data.id,
                               data.__tname__
@@ -815,7 +831,7 @@ const DataRowIndex = observer(function DataRowIndex({
                             state.parentObject!.subtypeName ??
                               state.parentObject!.objectTypeName,
                             state.parentObject!.fieldName,
-                            state.objectType!.name,
+                            state.objectType!,
                             state.insertedRows[rowIndex],
                             true
                           );

shared/studio/tabs/dataview/state/edits.ts
@@ -5,6 +5,7 @@ import {
   getNameOfSchemaType,
   SchemaType,
   escapeName,
+  SchemaObjectType,
 } from "@edgedb/common/schemaData";
 
 import {connCtx, dbCtx} from "../../../state";
@@ -197,7 +198,7 @@ export class DataEditingManager extends Model({}) {
     objectId: string | number,
     objectTypeName: string,
     fieldName: string,
-    linkTypeName: string,
+    linkType: SchemaObjectType,
     kind: UpdateLinkChangeKind,
     linkObjectId: string,
     linkObjectTypename: string
@@ -212,8 +213,7 @@ export class DataEditingManager extends Model({}) {
         escapedObjectTypeName: escapeName(objectTypeName, true),
         fieldName,
         escapedFieldName: escapeName(fieldName, false),
-        linkTypeName,
-        escapedLinkTypeName: escapeName(linkTypeName, true),
+        ...getLinkTypeName(linkType),
         changes: new Map(),
         inserts: new Set(),
       });
@@ -258,7 +258,7 @@ export class DataEditingManager extends Model({}) {
     objectId: string | number,
     objectTypeName: string,
     fieldName: string,
-    linkTypeName: string,
+    linkType: SchemaObjectType,
     insertedRow: InsertObjectEdit,
     setLink: boolean = false
   ) {
@@ -272,8 +272,7 @@ export class DataEditingManager extends Model({}) {
         escapedObjectTypeName: escapeName(objectTypeName, true),
         fieldName,
         escapedFieldName: escapeName(fieldName, false),
-        linkTypeName,
-        escapedLinkTypeName: escapeName(linkTypeName, true),
+        ...getLinkTypeName(linkType),
         changes: new Map(),
         inserts: new Set(),
       });
@@ -533,6 +532,24 @@ set {
 
     runInAction(() => this.propertyEdits.clear());
   }
+}
+
+function getLinkTypeName(type: SchemaObjectType): {
+  linkTypeName: string;
+  escapedLinkTypeName: string;
+} {
+  if (type.unionOf) {
+    return {
+      linkTypeName: `{${type.unionOf.map((utype) => utype.name).join(", ")}}`,
+      escapedLinkTypeName: `{${type.unionOf
+        .map((utype) => utype.escapedName)
+        .join(", ")}}`,
+    };
+  }
+  return {
+    linkTypeName: type.name,
+    escapedLinkTypeName: type.escapedName,
+  };
 }
 
 export function generateQueryFromStatements(

shared/studio/tabs/dataview/state/index.ts
@@ -152,7 +152,7 @@ export class DataView extends Model({
 
       const stackItem = this.inspectorStack[i + 1];
       const pointers = subtypeName
-        ? parentSchemaObject.descendents.find(
+        ? (parentSchemaObject.unionOf ?? parentSchemaObject.descendents).find(
             (desc) => desc.name === subtypeName
           )?.pointers
         : parentSchemaObject.pointers;

commit c89d87c438fe5f3150da2fb3103349a3109600d2

Fix regression introduced in #108 that breaks insert in dataview

shared/studio/tabs/dataview/dataInspector.tsx
@@ -423,10 +423,10 @@ const GridCell = observer(function GridCell({
       if (countData !== null || insertedRow) {
         const counts: {[typename: string]: number} =
           field.multi || !linkEditState
-            ? countData.reduce((counts: any, {typename, count}: any) => {
+            ? countData?.reduce((counts: any, {typename, count}: any) => {
                 counts[typename] = count;
                 return counts;
-              }, {})
+              }, {}) ?? {}
             : {};
 
         if (linkEditState) {