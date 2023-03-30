c13456b76ec986b4953ed72139d54533737c411e
James Clarke <james@edgedb.com>
2023-03-30 11:58:32 +0100

    Update select component to support nested groups and filtering + update dataview and querybuilder object select

---
+++ shared/studio/components/objectTypeSelect/index.tsx
@@ -0,0 +1,72 @@
+import {useMemo} from "react";
+
+import {SchemaObjectType} from "@edgedb/common/schemaData";
+import {Select, SelectItem, SelectItems} from "@edgedb/common/ui/select";
+
+export function ObjectTypeSelect({
+  className,
+  objectTypes,
+  selectedObjectType,
+  action,
+}: {
+  className?: string;
+  objectTypes: SchemaObjectType[];
+  selectedObjectType: SchemaObjectType;
+  action: (objectType: SchemaObjectType) => void;
+}) {
+  const objectSelectItems = useMemo(() => {
+    type groupMap = Map<string, {items: SelectItem[]; groups: groupMap}>;
+    const groups: groupMap = new Map();
+    for (const objectType of objectTypes) {
+      let current = {groups: groups} as {
+        items: SelectItem[];
+        groups: groupMap;
+      };
+      for (const mod of objectType.module.split("::")) {
+        if (!current.groups.has(mod)) {
+          current.groups.set(mod, {
+            items: [],
+            groups: new Map(),
+          });
+        }
+        current = current.groups.get(mod)!;
+      }
+      current.items.push({
+        id: objectType.name,
+        label: objectType.shortName,
+        fullLabel: objectType.name,
+        action: () => action(objectType),
+      });
+    }
+    const flattenMap = (groups: groupMap): SelectItems["groups"] => {
+      return [...groups.entries()].map(([name, group]) => ({
+        label: `${name}::`,
+        items: group.items,
+        groups: group.groups.size ? flattenMap(group.groups) : undefined,
+      }));
+    };
+    return groups.size === 1 && groups.has("default")
+      ? groups.get("default")!.items
+      : {
+          items: [],
+          groups: flattenMap(groups),
+        };
+  }, [objectTypes]);
+
+  return (
+    <Select
+      className={className}
+      title={
+        !Array.isArray(objectSelectItems) ? (
+          <>
+            <span style={{opacity: 0.65}}>{selectedObjectType.module}::</span>
+            {selectedObjectType.shortName}
+          </>
+        ) : undefined
+      }
+      items={objectSelectItems}
+      selectedItemId={selectedObjectType.name}
+      searchable
+    />
+  );
+}
