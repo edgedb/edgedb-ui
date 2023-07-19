e1931d36d69b2432cddda3e8b2816589a17a2387
James Clarke <james@edgedb.com>
2023-07-19 21:12:33 +0100

    Add wait to failing test

--- web/tests/simple.test.ts
+++ web/tests/simple.test.ts
@@ -3,7 +3,9 @@ import {ByUIClass, client, goToPage} from "./_utils";
 test("select version query", async () => {
   await goToPage("edgedb/editor");
 
-  const editor = await driver.findElement(By.className("cm-content"));
+  const editor = await driver.wait(
+    until.elementLocated(By.className("cm-content"))
+  );
   await editor.sendKeys("select sys::get_version_as_str()");
 
   const runButton = driver.findElement(ByUIClass("repl_runButton"));
