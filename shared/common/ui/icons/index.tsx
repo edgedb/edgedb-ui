195e59383190b7313898c042c0d560cf2ae73ee9
Dijana Pavlovic <dijana@edgedb.com>
2023-10-20 17:03:10 +0200

    Use camelCase for icon properties' names

--- shared/common/ui/icons/index.tsx
+++ shared/common/ui/icons/index.tsx
@@ -149,8 +149,8 @@ export function PlusIcon() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M5 0C4.44772 0 4 0.447716 4 1V4H1C0.447715 4 0 4.44771 0 5C0 5.55228 0.447716 6 1 6H4V9C4 9.55229 4.44772 10 5 10C5.55229 10 6 9.55228 6 9V6H9C9.55228 6 10 5.55229 10 5C10 4.44772 9.55228 4 9 4H6V1C6 0.447715 5.55229 0 5 0Z"
         fill="currentColor"
       />
