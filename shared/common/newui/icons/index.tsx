e3627ce9576b0b053bc57ee5080f8a219d3f36f7
James Clarke <james@edgedb.com>
2024-09-12 22:57:08 +0100

    Add circled check and cross icons

--- shared/common/newui/icons/index.tsx
+++ shared/common/newui/icons/index.tsx
@@ -526,3 +526,43 @@ export function MigrationsListIcon() {
     </svg>
   );
 }
+
+export function CircledCrossIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.5 8.5L12 12M12 12L15.5 15.5M12 12L15.5 8.5M12 12L8.5 15.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function CircledCheckIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16 10.0675L11.0038 15.0638L8.44 12.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
