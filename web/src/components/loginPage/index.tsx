071613edb12fda95c168edf16cd8b8dca6a18c99
James Clarke <james@edgedb.com>
2025-02-05 22:07:02 +0000

    Update to use scram implementation from edgedb-js + polish login page design

--- web/src/components/loginPage/index.tsx
+++ web/src/components/loginPage/index.tsx
@@ -1,18 +1,35 @@
 import {useEffect, useState} from "react";
 import {useForm} from "react-hook-form";
 
-import Button from "@edgedb/common/ui/button";
-import {ModalTextField} from "@edgedb/common/ui/modal";
+import {getHTTPSCRAMAuth} from "edgedb/dist/httpScram";
+import {cryptoUtils} from "edgedb/dist/browserCrypto";
+
+import {
+  ModalPanel,
+  ArrowRightIcon,
+  ModalContent,
+  TextInput,
+  SubmitButton,
+} from "@edgedb/common/newui";
 
 import {serverUrl, setAuthToken} from "../../state/models/app";
-import {SCRAMAuth} from "../../utils/scram";
-
-import styles from "./loginPage.module.scss";
-import {ArrowRight} from "@edgedb/studio/icons";
 import {Logo} from "../header";
 
+import styles from "./loginPage.module.scss";
+
+const httpSCRAMAuth = getHTTPSCRAMAuth(cryptoUtils);
+
+const isLocalhost = (() => {
+  try {
+    return new URL(serverUrl).hostname === "localhost";
+  } catch {
+    // ignore
+  }
+  return false;
+})();
+
 export default function LoginPage() {
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const {register, handleSubmit, formState, setFocus} = useForm<{
     username: string;
     password: string;
@@ -29,47 +46,61 @@ export default function LoginPage() {
   const onSubmit = handleSubmit(async ({username, password}) => {
     setError(null);
     try {
-      const authToken = await SCRAMAuth(serverUrl, username, password);
+      const authToken = await httpSCRAMAuth(serverUrl, username, password);
       setAuthToken(username, authToken);
     } catch (err) {
-      setError(err as Error);
       console.error(err);
+      setError("Login failed: username or password may be incorrect");
     }
   });
 
   return (
     <div className={styles.loginPage}>
-      <Logo className={styles.title} />
-      <form className={styles.loginForm} onSubmit={onSubmit}>
-        <ModalTextField
-          label="Username"
-          {...register("username", {
-            required: "Username is required",
-          })}
-          error={formState.errors.username?.message}
-        />
-        <ModalTextField
-          label="Password"
-          type="password"
-          {...register("password", {
-            required: "Password is required",
-          })}
-          error={formState.errors.password?.message}
-        />
-        <Button
-          className={styles.loginButton}
-          loading={formState.isSubmitting}
-          disabled={!formState.isValid || formState.isSubmitting}
-          label={"Login"}
-          icon={<ArrowRight className={styles.loginButtonIcon} />}
-          onClick={onSubmit}
-        />
-        {error ? (
-          <div className={styles.loginError}>
-            <span>{error.name}:</span> {error.message}
-          </div>
-        ) : null}
-      </form>
+      <Logo className={styles.logo} />
+
+      <ModalPanel
+        className={styles.loginPanel}
+        title="Welcome to Gel UI"
+        subheading={
+          isLocalhost ? (
+            <>
+              It looks like you're running Gel locally. You can also try
+              logging in by running the <code>gel ui</code> command from your
+              project directory.
+            </>
+          ) : undefined
+        }
+        onSubmit={onSubmit}
+        formError={error}
+        footerButtons={
+          <SubmitButton
+            kind="primary"
+            loading={formState.isSubmitting}
+            disabled={!formState.isValid || formState.isSubmitting}
+            rightIcon={<ArrowRightIcon />}
+          >
+            Login
+          </SubmitButton>
+        }
+      >
+        <ModalContent className={styles.formContent}>
+          <TextInput
+            label="Username"
+            {...register("username", {
+              required: "Username is required",
+            })}
+            error={formState.errors.username?.message}
+          />
+          <TextInput
+            label="Password"
+            type="password"
+            {...register("password", {
+              required: "Password is required",
+            })}
+            error={formState.errors.password?.message}
+          />
+        </ModalContent>
+      </ModalPanel>
     </div>
   );
 }
